e3d47e2cfe45fe654b6b686e29286eeb4fdfa17d
Korneliu <olbiton@gmail.com>
2020-02-25 23:15:25 -0800

    added handlebars and 404 page

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ const express = require('express')
 const app = express()
 
 const pubicDirectoryPath = path.join(__dirname, './public')
+const viewsPath = path.join(__dirname, './public/views')
 app.use(express.static(pubicDirectoryPath))
+app.use(express.static(viewsPath))
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-
+    res.render('about')
 })
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Max',
+        message: 'Page Not Found'
+
+
+    })
+})
+
+
 
 
 
