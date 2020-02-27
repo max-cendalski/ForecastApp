f78b7beec2c3f618da639cc3997654ba3b805843
Korneliu <olbiton@gmail.com>
2020-02-26 23:46:40 -0800

    added partials, changed styling

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+//Define paths for Express config
 const pubicDirectoryPath = path.join(__dirname, './public')
 const viewsPath = path.join(__dirname, './public/views')
+const partialsPath = path.join(__dirname, './public/partials')
+
+//Setup static directory to serve
 app.use(express.static(pubicDirectoryPath))
 app.use(express.static(viewsPath))
 
+//Setup Handlebars
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Forecast App',
+        name: 'Max'
+    })
 })
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Max'
+    })
+})
+
+
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {
+        title: 'About',
+        name: 'Max'
+    })
 })
 
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
         name: 'Max',
-        message: 'Page Not Found'
-
-
+        errorMessage: 'Page Not Found'
     })
 })
 
