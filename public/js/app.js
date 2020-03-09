e6ffb89ea7aa9c9cad253a44c8abb505475be79a
Korneliu <olbiton@gmail.com>
2020-03-09 00:00:31 -0700

    added event listener, changed index.hbs

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,22 @@ console.log('Client side javascript file is loaded')
     })
 }) */
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = searchElement.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
