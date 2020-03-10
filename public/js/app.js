eca7f793551f9ad15cf5161c091d2bf19a582462
Korneliu <olbiton@gmail.com>
2020-03-09 23:42:38 -0700

    added paragraphs to index.hbs and rendered forecast inside

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,11 @@ console.log('Client side javascript file is loaded')
 
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -17,10 +22,11 @@ weatherForm.addEventListener('submit', (event) => {
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
